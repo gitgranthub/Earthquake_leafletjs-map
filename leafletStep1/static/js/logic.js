f0fa2beb1fea6d8ad3213b9ef37f4a3c0b1cce18
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-10-07 11:01:18 -0500

    reorganizing script to my logic

--- leafletStep1/static/js/logic.js
+++ leafletStep1/static/js/logic.js
@@ -1,39 +1,7 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
-
-
-function createFeatures(earthquakeData) {
-  
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    // Setting the marker radius for the city by passing population into the markerSize function
-    
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "<hr>" + "<h3>" + "Magnitude" + "</h3>" + feature.properties.mag + "</p>");
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-  
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
+// CREATING THE MAP OBJECT
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
+  // Define darkmap and light map layers
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -79,3 +47,39 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 }
+
+
+
+// ACTUALLY BUILDING THE MAP FROM THE ABOVE FUNCTIONS
+function createFeatures(earthquakeData) {
+  
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    // Setting the marker radius for the city by passing population into the markerSize function
+    
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "<hr>" + "<h3>" + "Magnitude" + "</h3>" + feature.properties.mag + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+  
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
