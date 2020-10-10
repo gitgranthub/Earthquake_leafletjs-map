e60cd7c12925d890f560fba3fc8dd4822dea4783
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-10-10 11:15:58 -0500

    README update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # leaflet-challenge
 United States Geological Survey (USGS) earthquake data visualized, using leaflet mapping.
+![quake_site.png](leafletStep1/Images/quake_site.png)
+---
+* D3.js
+* leaflet
+* Mapbox
+* Javascript
+* HTML
--- leafletStep1/static/js/logic.js
+++ leafletStep1/static/js/logic.js
@@ -36,8 +36,8 @@ function createMap(earthquakes) {
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes,
-    //Magnitude: quakes
+    Earthquakes: earthquakes
+    // Plates: plateLayer
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -132,7 +132,7 @@ function createFeatures(geojson) {
 
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
+      
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
